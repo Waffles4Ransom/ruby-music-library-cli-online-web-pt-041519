df17c2e8afebe6aa2102cf97838370fa84daf9ea
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-08 23:54:36 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,7 +70,7 @@ class MusicLibraryController
     input_genre = gets.chomp
     if g = Genre.find_by_name(input_genre)
       g.songs.sort_by{|s| s.name}.each.with_index(1) do |song, i|
-      puts "#{i}. #{song.name} - #{song.name}"
+      puts "#{i}. #{song.artist.name} - #{song.name}"
       end 
     end 
   end 
