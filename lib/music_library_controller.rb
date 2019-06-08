7f64cf4efb9f13d95dda0e4c8bc0aacf6f9dd9c4
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-08 23:30:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -57,8 +57,8 @@ class MusicLibraryController
   
   def list_songs_by_artist
     puts "Please enter the name of a genre:"
-    input = get.chomp 
-    if input == 
+    input_artist = get.chomp 
+    artist = Artist.find_by_name
   end 
   
 end 
