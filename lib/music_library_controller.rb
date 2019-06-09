b32a637abb5cbd7399a3bec76054ccf2c799dc04
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-09 00:34:20 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -79,10 +79,9 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     song_input = gets.strip.to_i
     list = list_songs
-    binding.pry
     return if song_input < 1 || song_input > list.size 
     song_request = list[song_input - 1]
-    binding.pry
+
     if song_request
       puts "Playing #{song_request.name} by #{song_request.artist.name}"
     end
