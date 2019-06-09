bebadeb0da5cca20400965e4fa79c7c1aff380aa
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-09 00:05:44 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -77,7 +77,7 @@ class MusicLibraryController
   
   def play_song 
     puts "Which song number would you like to play?"
-    song_input = gets.chomp.to_i
+    song_input = gets.strip.to_i
     if song_input
   end 
   
